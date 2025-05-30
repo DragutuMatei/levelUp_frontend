c01eec6c2a14e2ac000ba7170b2acbf9b2fea386
matei <mateidr7@gmail.com>
2025-05-31 01:31:51 +0300

    asd

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../utils/AuthContext.js";
 import "../assets/css/style.scss";
+import { useState } from "react";
 
 function Home() {
   const { user, loading, signInWithGoogle, signUserOut } = useAuth(); // Get user, loading, AND the functions
   {
     /* {user && JSON.stringify(user)} */
   }
+
+  const [agree, setAgree] = useState(false);
 
   return (
     <div className="home">
@@ -29,10 +32,31 @@ function Home() {
             <h1>Logout</h1>
           </button>
         </>
+      ) : agree ? (
+        <>
+          <button onClick={signInWithGoogle} className="button main">
+            <h1>Logheaza-te pentru a incepe jocul</h1>
+          </button>{" "}
+        </>
       ) : (
-        <button onClick={signInWithGoogle} className="button main">
-          <h1>Logheaza-te pentru a incepe jocul</h1>
-        </button>
+        <>
+          <button className="button main">
+            <a
+              href="https://docs.google.com/document/d/1DgR-5dSoNMFG5MwgqcbMO-nWnfBOo1jVud3To8uJkkU/edit?usp=sharing"
+              target="_blank"
+            >
+              Regulament
+            </a>
+          </button>
+          <button
+            className="button second"
+            onClick={() => {
+              setAgree(true);
+            }}
+          >
+            <h1>Agree</h1>
+          </button>
+        </>
       )}
     </div>
   );
